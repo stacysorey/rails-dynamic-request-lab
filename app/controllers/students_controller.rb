38c77b719550dc72982157b3c29073b041114522
stacysorey <stacysorey@gmail.com>
2021-01-27 07:02:16 -0500

    commit tree 00856e735bd815f638d7bd6d50864f7baadb7ed2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
+  before_action :student, only: :show
+  
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def student
+    @student = Student.find(params[:id])
   end
 end
